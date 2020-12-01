e10f18772a8ab3664ec5bec40a6d520c78e32cee
Andrey Vorontsov <vorontsov180885@gmail.com>
2020-12-01 09:01:35 +0300

    обработал стандартную аналитику в таблицах

--- index.php
+++ index.php
@@ -8,31 +8,75 @@ try{
     die();
 }
 
-$sql = 'SELECT SUM(donates.price), COUNT(donates.price),ROUND(SUM(donates.price)/COUNT(donates.price),2) FROM donates WHERE (donates.`date`>=\'2020-11-16\');';
 
-vardump($dbh->query($sql));
-
-
+//1
+$sql = 'SELECT SUM(donates.price) AS \'sum\', COUNT(donates.price) AS \'count\',ROUND(SUM(donates.price)/COUNT(donates.price),2) AS \'averange\'  FROM donates WHERE (donates.`date`>=\'2020-11-16\');';
+//vardump($dbh->query($sql));
+echo "<table border =1><th>sum</th><th>count</th><th>averange</th>";
+echo "<caption>Общие сборы ночлежки после фильма Дудя</caption>";
 foreach ($dbh->query($sql) as $row) {
-    print $row['SUM(donates.price)'] . "\t";
-    print $row['COUNT(donates.price)'] . "\t";
-    print $row['ROUND(SUM(donates.price)/COUNT(donates.price),2)'] . "\n";
+    echo "<tr>";
+    echo "<td>". number_format($row['sum'], 2, ',', ' ') . "</td>";
+    echo "<td>". number_format($row['count'], 0, ',', ' ')  . "</td>";
+    echo "<td>". number_format($row['averange'], 2, ',', ' ')  . "</td>";
+    echo "</tr>";
 }
+echo "</table>";
+echo "<br><br>";
 
 
-$sql = 'SELECT projects.id, SUM(donates.price), COUNT(donates.price) FROM projects, donates WHERE ((donates.`date`>=\'2020-11-16\')AND(projects.id=donates.project_id)) GROUP BY projects.id;';
-vardump($dbh->query($sql));
-
+//2
+$sql = 'SELECT projects.`name` AS \'name\', SUM(donates.price) AS \'sum\', COUNT(donates.price) AS \'count\' FROM donates,projects WHERE ((donates.`date`>=\'2020-11-16\')AND(projects.id=donates.project_id)) GROUP BY projects.`name`;';
+//vardump($dbh->query($sql));
+echo "<table border =1><th>name</th><th>sum</th><th>count</th>";
+echo "<caption>Общие сборы ночлежки после фильма Дудя в разрезе проектов</caption>";
 foreach ($dbh->query($sql) as $row) {
-    print $row['projects.id'] . "\t";
-    print $row['SUM(donates.price)'] . "\t";
-    print $row['COUNT(donates.price)'] . "<br>";
+    echo "<tr>";
+    echo "<td>". $row['name'] . "</td>";
+    echo "<td>". number_format($row['sum'], 2, ',', ' ')  . "</td>";
+    echo "<td>". number_format($row['count'], 0, ',', ' ')  . "</td>";
+    echo "</tr>";
 }
+echo "</table>";
+echo "<br><br>";
 
 
-$sql = 'SELECT donates.`date`, SUM(donates.price),COUNT(donates.price),ROUND(SUM(donates.price)/COUNT(donates.price),2) AS averange FROM donates WHERE donates.`date`>\'2020-10-01\' GROUP BY donates.`date` order BY donates.`date`;';
-vardump($dbh->query($sql));
 
-$sql = 'SELECT donates.`name`, sum(donates.price) FROM donates WHERE (donates.price>=50000) GROUP BY donates.`name` ORDER BY sum(donates.price) desc;';
-vardump($dbh->query($sql));
+
+
+
+
+
+//3
+
+$sql = 'SELECT DATE_FORMAT(donates.`date`, \'%d.%m.%Y\') AS \'date\', SUM(donates.price) AS \'sum\',COUNT(donates.price) AS \'count\',ROUND(SUM(donates.price)/COUNT(donates.price),2) AS \'averange\' FROM donates WHERE donates.`date`>\'2020-10-01\' GROUP BY donates.`date` order BY donates.`date`;';
+//vardump($dbh->query($sql));
+echo "<table border =1><th>date</th><th>sum</th><th>count</th><th>averange</th>";
+echo "<caption>Смотрим динамику пожертвований чуть до и после фильма Дудя</caption>";
+foreach ($dbh->query($sql) as $row){
+    echo "<tr>";
+    echo "<td>". $row['date']. "</td>";
+    echo "<td>". number_format($row['sum'], 2, ',', ' ') . "</td>";
+    echo "<td>". number_format($row['count'], 0, ',', ' ')  . "</td>";
+    echo "<td>". number_format($row['averange'], 2, ',', ' ')  . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "</table>";
+echo "<br><br>";
+
+
+//4
+$sql = 'SELECT donates.`name` AS \'name\', sum(donates.price) AS \'sum\' FROM donates WHERE (donates.price>=50000) GROUP BY donates.`name` ORDER BY sum(donates.price) desc;';
+//vardump($dbh->query($sql));
+echo "<table border =1><th>name</th><th>sum</th>";
+echo "<caption>максимальные пожертвования</caption>";
+foreach ($dbh->query($sql) as $row){
+    echo "<tr>";
+    echo "<td>". $row['name']. "</td>";
+    echo "<td>". number_format($row['sum'], 2, ',', ' ') . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
 
